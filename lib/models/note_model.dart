fca9942785408499ae4b3692cb1a3de50a54b433
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2023-12-24 18:12:22 +0200

    create type adaptor for note

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,20 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+//Dart file name to the part directive, often triggered by the build_runner package
+
+@HiveType(typeId: 0)
+//uique per model ,it should be unique for each class marked with @HiveType.
+
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  //uique per class, Each field that is going to be stored needs a unique HiveField identifier.
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final String color;
 
   NoteModel(
