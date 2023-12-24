91d02225e7f94c8dedb691616b91deaea53533c6
moelhewehy7 <m.osama_el7ewehy@yahoo.com>
2023-12-24 20:56:27 +0200

    register daptor

---
+++ lib/constants.dart
@@ -0,0 +1,1 @@
+String Knotesbox = "notes_box";
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/home_page.dart';
+
+import 'constants.dart';
 
 void main() async {
   await Hive.initFlutter(); //dontforget hive flutter not just hive
-  await Hive.openBox("notes_box");
+  await Hive.openBox(Knotesbox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NoteApp());
 }
 
